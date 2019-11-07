6b98012da4565135ab4882ea47de2b5074aa10c7
Yi-Hsuan <amity3744039@gmail.com>
2019-11-07 17:43:40 +0800

    newfile Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def times(a, b):
+    return a * b
+
+def divided(a, b):
+    if b != 0:
+        return a / b
+    return "無限"
